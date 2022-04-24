e8060a8377876420fd63d9b74296c69b99fdf061
Daniel-Budd <97355374+Daniel-Budd@users.noreply.github.com>
2022-04-24 13:28:33 -0500

    Started razor class library

--- Projects/Main/Program.cs
+++ Projects/Main/Program.cs
@@ -6,8 +6,5 @@ Equation e1 = (n) => 5.0 * n;
 Equation main1 = e1.Modify("^",2.0);
 Equation poly1 = new Polynomial((3,4),(2,3),(1,1),(5,3));
 Console.WriteLine("Enumerating over main1.");
-foreach(Number @value in main1.Enumerate(0.0,3.0,0.25))
-{
-  Console.WriteLine(@value);
-}
 void Log(Number n) => Console.WriteLine(n.ToString());
+main1.Enumerate(-10.0,10.0,0.5).ForEach(Log);
---
+++ RazorLibraries/RazorLibraries.General/VNumber.razor.cs
@@ -0,0 +1,9 @@
+namespace RazorLibraries.General;
+using Libraries;
+public partial class VNumber
+{
+    protected override async Task OnInitializedAsync()
+    {
+        // 
+    }
+}
